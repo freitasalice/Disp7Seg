dff4a5dfdb6f84a35fce5e13dc3ba76ec91a5f05
21192692 <21192692@DSN1271158630.cfp127aluno.local>
2021-02-26 16:41:40 -0300

    Maquinas de estados para enclementar botão de adição e subtração.

--- main.c
+++ main.c
@@ -10,20 +10,46 @@
 #include "config.h"
 #include "delay.h"
 #include "disp7seg.h"
+#include "botoes.h"
 
 
 void main(void) 
 {
-    int cont = 0;
-    disp7seg_init ();
-    
-    while ( 1 )
-    {
-        display7seg ( cont );
-        delay ( 1000 );
-        cont++;
-        if ( cont >= 16 )
-            cont = 0;
+    char estado = 0;
+     d7seg.init();
+     botoes_init();
         
+    while ( 1 )
+    {   
+        switch (estado)
+        {
+            case 0:
+                estado = 1;
+                break;
+             
+        
+            case 1:
+                if (adicionar() == 1 );
+                    estado = 2;
+                    break;
+             
+            case 2:
+               if (adicionar() == 0 );
+               estado = 3;
+                break;
+                
+            case 3: 
+                ++cont;
+                estado = 0;
+                break;        
+                       
+        }
+        d7seg.print(cont);
+        
+        if( cont >= 10)
+            cont = 0;
+        if( cont < 0)
+            cont = 9;
+                      
     }
 }
